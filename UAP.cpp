83fc452528513fcb8182cd484582efcdb5061c5f
Abdilah Asyidiqi <70487624+asid30@users.noreply.github.com>
2022-06-21 13:29:28 +0700

    Update UAP.cpp

--- UAP.cpp
+++ UAP.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <queue>
-#include "windows.h"
 #include <conio.h>
 #define max 50
 
@@ -10,49 +9,38 @@ queue <int> antrian;
 int order = 1;
 
 void menu1(){
-    if(antrian.size() >= max)	cout<< "Antrian penuh, sampai bertemu di antrian besok >/<";
+    if(antrian.size() == max)   cout<< "Antrian penuh";
     else {
-		if(order <= max){
+		while(order != max){
 			antrian.push(order);
-		} else {
-			cout<< "Antrian penuh, sampai bertemu di antrian besok >/<";
-			getch();
-			exit(0);
+			break;
 		}
 		order++;
-		cout<<"Antrian nomor <" <<antrian.back()<<"> telah masuk";
+		cout<<"Antrian nomor "<<antrian.back()<<" telah masuk";
 	}
+	
 }
 
 void menu2(){
     if(antrian.empty())    cout<<"Antrian kosong";
 	else{
-		cout<<"Antrian nomor <"<<antrian.front()<<"> telah selesai, \nsilahkan untuk nomer <" << antrian.front()+1 << "> untuk berkonsultasi dengan customer service kami >/<";
+		cout<<"Antrian nomor "<<antrian.front()<<" telah keluar";
 	    antrian.pop();
 	}
 }
 
-void menu3(){
-	if(antrian.empty())		cout<<"Antrian sudah kosong";
+void view(queue <int> antrian_update){
+    queue <int> antri = antrian_update;
+    cout<<"<<Update antrian>>"<<endl;
+    if(antri.empty())	cout<<"\nAntrian kosong\n\n";
 	else{
-		while(!antrian.empty())		antrian.pop();
-		order = 1;
-		cout<<"Selamat datang di hari selanjutnya >/<"<<endl;
-	}
-}
-
-void view(queue <int> update){
-    queue <int> antri = update;
-    cout<<"<<Update antrian>>" << endl;
-    if(antri.empty())	cout << "\nAntrian kosong\n\n";
-	else{
-		cout << antri.front()<<endl<<endl;
-		cout << "No. Antrian Berikutnya\n";
+		cout<<antri.front()<<endl<<endl;
+		cout<<"No. Antrian Berikutnya\n";
 		antri.pop();
 		if(antri.empty())   cout<<"[Kosong]";
 		else{
 		    while(!antri.empty()) {
-		        cout << antri.front() << "\t";
+		        cout<<antri.front()<<"\t";
 		        antri.pop();
 		    }
 		}
@@ -65,36 +53,29 @@ int main(){
     int menu;
 
     do{
-		system("cls");
         view(antrian);
         cout << "\n\t Selamat Datang di Bank NGAB\n\n";
         cout << "Pilih Menu\n";
         cout << "1. Masukan antrian\n";
         cout << "2. Antrian selanjutnya\n";
-		cout << "3. Reset harian\n";
-        cout << "4. Exit\n\n";
+        cout << "3. Exit\n";
 
         cin >> menu;
 
         switch(menu){
 			case 1 :
 				menu1();
-				Sleep(700);
+				getch();
 				break;
 
 			case 2 :
 				menu2();
-				Sleep(1000);
+				getch();
 				break;
 
 			case 3:
-				menu3();
-				Sleep(700);
-				break;
-
-			case 4:
 				cout<<"Terimakasih sudah menggunakan layanan kami >.<"<<endl;
-				Sleep(1000);
+				getch();
 				exit(0);
 
 			default:
@@ -104,6 +85,6 @@ int main(){
 		}
 		system("cls");
         
-    }while(menu > 0 || menu < 5);
+    }while(menu >= 0 || menu <= 4);
     return 0;
 }
